a361c2f4433bbb63e9493ba4232f04f488c46c6f
Thomas Sutton <me@thomas-sutton.id.au>
2010-11-27 23:51:27 +0800

    Version 0.1

--- Blaze/ByteString/Builder/Enumerator.hs
+++ Blaze/ByteString/Builder/Enumerator.hs
@@ -6,7 +6,8 @@
 -- Stability    : Experimental
 -- Portability  : Unknown
 --
--- Wrap 
+-- Simplify the process of using @blaze-builder@ with @enumerator@ by 
+-- converting functions that construct 'Builder's into 'Iteratee's.
 ------------------------------------------------------------------------------
 
 module Blaze.ByteString.Builder.Enumerator where
@@ -16,12 +17,12 @@ import qualified Data.ByteString as B
 import           Data.Enumerator
 
 -- | Convert a function that returns a @blaze-builder@ 'Builder' into an 
--- 'Iteratee'. Each incoming value will be converted into a lazy 'ByteString'.
+-- 'Iteratee'. Each incoming value will be converted into a 'ByteString'.
 iterBuilder :: Monad m => (a -> Builder) -> Iteratee a m B.ByteString
-iterBuilder build = continue (step build)
-
-step :: Monad m => (a -> Builder) -> Stream a -> Iteratee a m B.ByteString
-step build input = case input of
-  EOF           -> yield (B.empty) EOF
-  Chunks    []  -> continue (step build)
-  Chunks (v:vs) -> yield (toByteString $ build v) (Chunks vs)
+iterBuilder builder = continue (step builder)
+ where
+   step :: Monad m => (a -> Builder) -> Stream a -> Iteratee a m B.ByteString
+   step b input = case input of
+     EOF           -> yield (B.empty) EOF
+     Chunks    []  -> continue (step b)
+     Chunks (v:vs) -> yield (toByteString $ b v) (Chunks vs)
